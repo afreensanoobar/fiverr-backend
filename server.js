e2493db4c425fa89f65fbe45de23c45d125c3ac6
afreensanoobar <afreensanoobar0026@gmail.com>
2026-01-23 23:58:35 +0530

    update

--- server.js
+++ server.js
@@ -1,29 +1,17 @@
 // const express = require("express")
 // const app = express()
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import "dotenv/config.js";
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+import "dotenv/config.js"
 
-dotenv.config();
+dotenv.config()
 
-import express from "express";
+import express from "express"
 const app = express()
 app.listen(3000, () => {
   console.log("Server is running on port 3000")
-});
-import userRoutes from "./routes/userRoutes.js";
+})
+import userRoutes from "./routes/userRoutes.js"
 
-app.use(express.json());
-app.use("/api/users", userRoutes);
-
-const connect a ()
-try{
-  await mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-  console.log("Connected to MongoDB");
-} catch (error) {
-  console.error("Error connecting to MongoDB:", error);
-}
-
+app.use(express.json())
+app.use("/api/users", userRoutes)
