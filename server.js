1a7aa36d2b4b6c43121d69ee2597526f1d41fcfe
afreensanoobar <afreensanoobar0026@gmail.com>
2026-01-25 21:12:18 +0530

    new file added

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 // const express = require("express")
 // const app = express()
 import mongoose from "mongoose"
+import express from "express"
 import dotenv from "dotenv"
 import "dotenv/config.js"
 
+const app = express()
 dotenv.config()
 
-import express from "express"
-const app = express()
+mongoose.set("strictQuery", true)
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO)
+    console.log("Connected to MongoDB")
+  } catch (err) {
+    console.log("Error connecting to MongoDB:", err)
+  }
+}
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000")
 })
-import userRoutes from "./routes/userRoutes.js"
-
-app.use(express.json())
-app.use("/api/users", userRoutes)
