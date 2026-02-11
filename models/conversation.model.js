ffe2ee6bef0f6d5bb38a463799daeb5aa53950c8
afreensanoobar <afreensanoobar0026@gmail.com>
2026-02-11 21:17:12 +0530

    p

--- models/conversation.model.js
+++ models/conversation.model.js
@@ -1,1 +1,37 @@
-i
+import mongoose from "mongoose"
+const { Schema } = mongoose
+
+const ConversationSchema = new Schema(
+  {
+    id: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    sellerId: {
+      type: String,
+      required: true,
+    },
+    buyerId: {
+      type: String,
+      required: true,
+    },
+    readBySeller: {
+      type: Boolean,
+      required: true,
+    },
+    readByBuyer: {
+      type: Boolean,
+      required: true,
+    },
+    lastMessage: {
+      type: String,
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+)
+
+export default mongoose.model("Conversation", ConversationSchema)
